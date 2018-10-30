cf8355d82faa5f8a27cae109a2b4549c5d7fe1ed
Zachary Lee <zacharylhlee@gmail.com>
2018-10-30 15:21:54 +0000

    Added componentDidMount function in App

--- src/App.jsx
+++ src/App.jsx
@@ -9,18 +9,29 @@ class App extends Component {
       currentUser: {name: "Bob"}, //Optional
       messages: [
         {
-          id: "01",
+          id: 1,
           username: "Bob",
           content: "Has anyone seen my marbles?",
         },
         {
-          id: "02",
+          id: 2,
           username: "Anonymous",
           content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
         },
       ]
     }
   }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage);
+      this.setState({messages: messages});
+    }, 3000);
+  }
+
   render() {
     return (
       <div>
