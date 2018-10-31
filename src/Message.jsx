84238684e62a910654d6944899d09e11365bc34e
Zachary Lee <zacharylhlee@gmail.com>
2018-10-31 21:53:03 +0000

    Adds function to display image if image URL is included in message

--- src/Message.jsx
+++ src/Message.jsx
@@ -1,11 +1,29 @@
 import React, {Component} from "react";
 
+function includesImgURL(entry){
+  const ImgURLRegularExpression =  /([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i;
+  return ImgURLRegularExpression.test(entry);
+}
+
 function Message({user, content, color}) {
-  return (
-    <div className="message">
-      <span className="message-username" style={{color: color}}>{user}</span>
-      <span className="message-content">{content}</span>
-    </div>
-  );
+  if (includesImgURL(content)) {
+    const rx = /([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i;
+    const imgURL = rx.exec(content);
+    const newContent = content.replace(imgURL[0], "")
+    return (
+      <div className="message">
+        <span className="message-username" style={{color: color}}>{user}</span>
+        <img className="message-image" src={imgURL[0]} />
+        <span className="message-content">{newContent}</span>
+      </div>
+      )
+  } else {
+    return (
+      <div className="message">
+        <span className="message-username" style={{color: color}}>{user}</span>
+        <span className="message-content">{content}</span>
+      </div>
+    );
+  }
 }
 module.exports = Message;
