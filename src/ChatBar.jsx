2a0c20463ce917d574257c630eaf86c8da2ad8d9
Zachary Lee <zacharylhlee@gmail.com>
2018-10-31 17:12:35 +0000

    Refactor ChatBar to submit message when when onKeyPress is Enter

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,21 +1,17 @@
 import React, {Component} from "react";
 
 function ChatBar({user, newMessage, changeUsername}) {
-
-  const onSubmit = (event) => {
-    event.preventDefault();
-    const content = event.target.elements.content;
-    newMessage(content.value)
-    content.value = "";
+  const submitMessage = (event) => {
+    if (event.key === 'Enter') {
+      newMessage(event.target.value)
+      event.target.value = "";
     }
+  }
 
   return (
     <footer className="chatbar">
-      <form className="chatbar" onSubmit={onSubmit}>
-        <input type="text" className="chatbar-username" placeholder="Your Name (Optional)" onChange={changeUsername} />
-        <input type="text" name="content" className="chatbar-message" placeholder="Type a message and hit ENTER" />
-        <button type="submit" style={{display: "None"}}></button>
-      </form>
+      <input type="text" className="chatbar-username" placeholder="Your Name (Optional)" onChange={changeUsername} />
+      <input type="text" name="content" className="chatbar-message" placeholder="Type a message and hit ENTER" onKeyPress={submitMessage} />
     </footer>
   );
 }
