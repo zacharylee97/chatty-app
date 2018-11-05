e634ab939a0344ac7f68b583e42b8f591680c626
Zachary Lee <zacharylhlee@gmail.com>
2018-11-05 22:11:34 +0000

    Adds function to create message with image

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,22 +5,26 @@ function includesImgURL(entry){
   return ImgURLRegularExpression.test(entry);
 }
 
+function messageWithImgURL(user, content, color) {
+  const rx = /([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i;
+  const imgURL = rx.exec(content);
+  const newContent = content.replace(imgURL[0], "")
+  return (
+    <div className="message">
+      <span className="message-username" style={{color: color}}>{user}</span>
+      <span className="message-content">
+        {newContent}
+        <br />
+        <img className="message-image" src={imgURL[0]} />
+      </span>
+
+    </div>
+  )
+}
+
 function Message({user, content, color}) {
   if (includesImgURL(content)) {
-    const rx = /([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i;
-    const imgURL = rx.exec(content);
-    const newContent = content.replace(imgURL[0], "")
-    return (
-      <div className="message">
-        <span className="message-username" style={{color: color}}>{user}</span>
-        <span className="message-content">
-          {newContent}
-          <br />
-          <img className="message-image" src={imgURL[0]} />
-        </span>
-
-      </div>
-      )
+    return messageWithImgURL(user, content, color)
   } else {
     return (
       <div className="message">
